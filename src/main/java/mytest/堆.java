24a190bc4b616b133c93aa7c5272a1c2213ec04c
liuchuanlll <1476593541@qq.com>
2023-03-24 13:00:26 +0800

    堆

--- src/main/java/mytest/堆.java
+++ src/main/java/mytest/堆.java
@@ -7,13 +7,23 @@ package mytest;
  * @Description
  */
 public class 堆 {
-//    存储：顺序存储或者链接存储，但由于堆是一个完全二叉树，所以宜采用顺序存储
-//    1、有限数组形式：int heap[1024]    heap[i*2+1] heap[i*2+2]为左右子树 heap[i/2]为父节点
-//    2.动态数组
-    class Heap<T>{
-        T[] arr;
-        int len;//堆长度或堆元素个数
-        int maxSize;//动态数组空间大小，虽然[0···maxSize-1]可能存在数据，但只有[0···len-1]存放堆有效元素
-    }
+    Heap heap;
 //1、初始化堆
+    void initHeap(Heap heap) {
+        this.heap = heap;
+    }
+//插入元素
+
+}
+//    存储：顺序存储或者链接存储，但由于堆是一个完全二叉树，所以宜采用顺序存储 heap[i*2+1] heap[i*2+2]为左右子树 heap[i/2]为父节点
+class Heap{
+    private int[] heapArray;
+    private int maxSize;//动态数组空间大小，虽然[0···maxSize-1]可能存在数据，但只有[0···len-1]存放堆有效元素
+    private int currentSize;//堆长度或堆元素个数
+
+    public Heap (int[] n) {
+        maxSize = n.length;
+        currentSize = n.length;
+        heapArray = n;
+    }
 }
