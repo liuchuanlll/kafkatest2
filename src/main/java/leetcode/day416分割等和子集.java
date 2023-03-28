8f7b27052c079cb7923463bc7fd8c69ab0f38cd6
刘川 <liuc@sdc.icbc.com.cn>
2023-03-28 12:39:59 +0800

    背包问题 + 零钱兑换+分割等和子集

---
+++ src/main/java/leetcode/day416分割等和子集.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/**
+ * @author kfzx-liuc02
+ * @version 1.0
+ * @date 2023/3/28 10:48
+ * @Description
+ */
+public class day416分割等和子集 {
+//    一、暴力递归自下而上
+    public boolean canPartition(int[] nums) {
+        int sum=0;
+        for(int n:nums){
+            sum+=n;
+        }
+        if(sum%2!=0) return false;
+        sum=sum/2;
+        boolean b = canPartition(nums, sum, 0, 0, false);
+        return b;
+    }
+    public boolean canPartition(int[] nums,int sum,int total,int index,boolean exist) {
+        if(sum==total||exist==true) return true;
+        if(total>sum) return false;
+        for(int i=index;i<nums.length;i++){
+            total+=nums[i];
+            exist = canPartition(nums, sum, total, index + 1,exist)||exist;//此处||exist可以不要
+        }
+        return exist;
+    }
+    //    二、暴力递归自上而下 b[i][sum]=b[i-1][sum]||b[i][sum-n[i]]
+    public boolean canPartition2(int[] nums) {
+        int sum=0;
+        for(int n:nums){
+            sum+=n;
+        }
+        if(sum%2!=0) return false;
+        sum=sum/2;
+        boolean b = canPartition2(nums, sum, 0, 0, false);
+        return b;
+    }
+}
