8b5c84a7f3d48488642747a58666ec579d451676
刘川 <liuc@sdc.icbc.com.cn>
2023-03-28 19:52:27 +0800

    背包问题 + 零钱兑换+分割等和子集

--- src/main/java/leetcode/day416分割等和子集.java
+++ src/main/java/leetcode/day416分割等和子集.java
@@ -7,7 +7,7 @@ package leetcode;
  * @Description
  */
 public class day416分割等和子集 {
-//    一、暴力递归自下而上
+//    一、暴力回溯自下而上
     public boolean canPartition(int[] nums) {
         int sum=0;
         for(int n:nums){
@@ -35,7 +35,48 @@ public class day416分割等和子集 {
         }
         if(sum%2!=0) return false;
         sum=sum/2;
-        boolean b = canPartition2(nums, sum, 0, 0, false);
+        boolean b = canPartition2(nums, sum, nums.length-1);
         return b;
+    }
+
+    private boolean canPartition2(int[] nums, int remain, int i) {
+        if(remain<0||i<0) {
+            return false;
+        }
+        boolean b = canPartition2(nums, remain, i - 1) || canPartition2(nums, remain - nums[i], i - 1);
+        return b;
+    }
+
+    boolean exist[][];
+    public boolean canPartition3(int[] nums) {
+        int sum=0;
+        for(int n:nums){
+            sum+=n;
+        }
+        if(sum%2!=0) return false;
+        sum=sum/2;
+        exist=new boolean[nums.length][sum+1];
+        boolean b = canPartition3(nums, sum, nums.length-1);
+        return b;
+    }
+
+    private boolean canPartition3(int[] nums, int remain, int i) {
+        if(remain<0||i<0) {
+            return false;
+        }
+        if(exist[i][remain]==true) return true;//递归搜索多加这一句
+        exist[i][remain]=canPartition3(nums, remain, i-1)||canPartition3(nums,remain-nums[i], i-1);
+        return exist[i][remain];
+    }
+    //动态规划：b[i][sum]=b[i-1][sum]||b[i][sum-n[i]]
+    public boolean canPartition4(int[] nums) {
+        int sum=0;
+        for(int n:nums){
+            sum+=n;
+        }
+        if(sum%2!=0) return false;
+        sum=sum/2;
+        boolean[][] exist2=new boolean[nums.length][sum+1];
+        for(int i=0;i<)
     }
 }
